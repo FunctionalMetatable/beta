17612b616933f28e7806f9da5ae8778b2227b80c
9gr <67440879+FunctionalMetatable@users.noreply.github.com>
2021-03-31 12:10:23 +0100

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,10 @@
+fetch(
+  "https://stats.9gr.repl.co/github/website/analytics/addviews/" +
+    btoa("functionalmetatable.github.io" + window.location.pathname)
+)
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(
+      "[analytics]: added views, from server got " + JSON.stringify(res)
+    );
+  });
