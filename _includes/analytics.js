0dd94d25993c68a29df5e6a1140990f422557b78
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-03-31 11:06:17 +0000

    run prettier [auto]

--- _includes/analytics.js
+++ _includes/analytics.js
@@ -1,5 +1,10 @@
-fetch("https://stats.9gr.repl.co/github/website/analytics/addviews/" + atob("functionalmetatable.github.io"+ window.location.pathname))
-.then(res => res.json())
-.then(res => {
-  console.log("[analytics]: added views, from server got " + JSON.stringify(res))
-})
+fetch(
+  "https://stats.9gr.repl.co/github/website/analytics/addviews/" +
+    atob("functionalmetatable.github.io" + window.location.pathname)
+)
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(
+      "[analytics]: added views, from server got " + JSON.stringify(res)
+    );
+  });
