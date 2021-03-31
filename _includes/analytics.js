0f7dd6864b2299076ff749b31e4a2e10610af296
9gr <67440879+FunctionalMetatable@users.noreply.github.com>
2021-03-31 12:05:59 +0100

    Create analytics.js

---
+++ _includes/analytics.js
@@ -0,0 +1,5 @@
+fetch("https://stats.9gr.repl.co/github/website/analytics/addviews/" + atob("functionalmetatable.github.io"+ window.location.pathname))
+.then(res => res.json())
+.then(res => {
+  console.log("[analytics]: added views, from server got " + JSON.stringify(res))
+})
