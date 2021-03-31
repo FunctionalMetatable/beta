cfc32d773ea0949b2eacd828ad4fb0e90042a813
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-03-31 13:10:44 +0000

    run prettier [auto]

--- analytics.js
+++ analytics.js
@@ -1,7 +1,9 @@
-link = "functionalmetatable.github.io" + window.location.pathname + ( (new URL(window.location.href)).search || "" )
+link =
+  "functionalmetatable.github.io" +
+  window.location.pathname +
+  (new URL(window.location.href).search || "");
 fetch(
-  "https://stats.9gr.repl.co/github/website/analytics/addviews/" +
-    btoa(link)
+  "https://stats.9gr.repl.co/github/website/analytics/addviews/" + btoa(link)
 )
   .then((res) => res.json())
   .then((res) => {
