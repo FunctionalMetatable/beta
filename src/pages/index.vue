66f9e9047565768e9ea29d67e980e4ea587b6ca6
FunctionalMetatable <>
2021-05-30 10:30:00 +0000

    asdf

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,28 +1,7 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">site</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <Header />
+  <h1>nuxt nuxt nuxt nuxt nuxt</h1>
+  <Footer />
 </template>
 
 <script>
